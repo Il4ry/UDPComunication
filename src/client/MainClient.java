e94b6f0e7eeeb2f6edd166d56db97e47de18ef23
Il4ry <ilariatenchini@gmail.com>
2026-03-15 20:00:36 +0100

    scambio di messaggi

--- src/client/MainClient.java
+++ src/client/MainClient.java
@@ -9,24 +9,29 @@ import java.util.Scanner;
 public class MainClient {
     public static void main(String[] args) {
         System.out.println("CLIENT");
-        try {
-            InetAddress serverAddress = InetAddress.getLocalHost();
-            System.out.println("Indirizzo del server trovato!");
-            DatagramSocket dSocket = new DatagramSocket();
-            //si prepara il datagramma con i dati da inviare
-            String message ="ciao";
-            //prendo i bit, la lunghezza del messaggio, l'indirizzo del server e la porta
-            DatagramPacket outPacket = new DatagramPacket(message.getBytes(), message.length(), serverAddress, 3000);
-            //si inviano i dati
-            dSocket.send(outPacket);
-            byte[] bufferIn = new byte[256];
-            DatagramPacket inputPacket = new DatagramPacket(bufferIn, bufferIn.length);
-            dSocket.receive(inputPacket);
-            String Servermessage= new String (inputPacket.getData(), 0, inputPacket.getLength());
-            System.out.println(Servermessage);
-        }catch(IOException e){
-            System.out.println("Errore");
-        }
+
+            try {
+                InetAddress serverAddress = InetAddress.getLocalHost();
+                System.out.println("Indirizzo del server trovato!");
+                while(true) {
+                    DatagramSocket dSocket = new DatagramSocket();
+                    //si prepara il datagramma con i dati da inviare
+                    System.out.println("Scrivi il messaggio da voler mandare al server: ");
+                    Scanner sc = new Scanner(System.in);
+                    String message = sc.nextLine();
+                    //prendo i bit, la lunghezza del messaggio, l'indirizzo del server e la porta
+                    DatagramPacket outPacket = new DatagramPacket(message.getBytes(), message.length(), serverAddress, 3000);
+                    //si inviano i dati
+                    dSocket.send(outPacket);
+                    byte[] bufferIn = new byte[256];
+                    DatagramPacket inputPacket = new DatagramPacket(bufferIn, bufferIn.length);
+                    dSocket.receive(inputPacket);
+                    String Servermessage = new String(inputPacket.getData(), 0, inputPacket.getLength());
+                    System.out.println("Server: " + Servermessage);
+                }
+            } catch (IOException e) {
+                System.out.println("Errore");
+            }
 
     }
 }
--- src/server/MainServer.java
+++ src/server/MainServer.java
@@ -4,26 +4,33 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Scanner;
 
 public class MainServer {
     public static void main(String[] args) {
         System.out.printf("SERVER");
 
-        try {
-            DatagramSocket dSocket = new DatagramSocket(3000);
-            byte[] bufferIn = new byte[256];
-            DatagramPacket inputPacket = new DatagramPacket(bufferIn, bufferIn.length);
-            dSocket.receive(inputPacket);
-            String message= new String (inputPacket.getData(), 0, inputPacket.getLength());
-            System.out.println("\nClient: " + message);
-            InetAddress clientAddress = inputPacket.getAddress();
-            int port = inputPacket.getPort();
-            DatagramPacket outPacket =  new DatagramPacket(message.getBytes(), message.length(), clientAddress, port);
-            dSocket.send(outPacket);
-            System.out.println("messaggio rinviato");
-        } catch (IOException e) {
-            System.out.println("Errore");
-        }
+            try {
+                DatagramSocket dSocket = new DatagramSocket(3000);
+                byte[] bufferIn = new byte[256];
+                DatagramPacket inputPacket = new DatagramPacket(bufferIn, bufferIn.length);
+                while(bufferIn.length!=0) {
+                    dSocket.receive(inputPacket);
+                    String clientMessage = new String(inputPacket.getData(), 0, inputPacket.getLength());
+                    System.out.println("\nClient: " + clientMessage);
+                    InetAddress clientAddress = inputPacket.getAddress();
+                    int port = inputPacket.getPort();
+                    System.out.println("Scrivi il messaggio da voler mandare al client: ");
+                    Scanner sc = new Scanner(System.in);
+                    String message = sc.nextLine();
+                    //prendo i bit, la lunghezza del messaggio, l'indirizzo del server e la porta
+                    DatagramPacket outPacket = new DatagramPacket(message.getBytes(), message.length(), clientAddress, port);
+                    dSocket.send(outPacket);
+                }
+                System.out.println("Fine trasmissione");
+            } catch (IOException e) {
+                System.out.println("Errore");
+            }
 
     }
 }
