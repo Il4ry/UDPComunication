0b6094220b5cbd4583c298de5132ea7396477723
diegoscuolaitis-tech <diegoscuolaitis@gmail.com>
2026-03-16 12:52:32 +0100

    consegna 16/03

--- src/server/MainServer.java
+++ src/server/MainServer.java
@@ -1,9 +1,7 @@
 package server;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.net.*;
 import java.util.Scanner;
 
 public class MainServer {
@@ -15,19 +13,26 @@ public class MainServer {
                 byte[] bufferIn = new byte[256];
                 DatagramPacket inputPacket = new DatagramPacket(bufferIn, bufferIn.length);
                 while(bufferIn.length!=0) {
-                    dSocket.receive(inputPacket);
-                    String clientMessage = new String(inputPacket.getData(), 0, inputPacket.getLength());
+                    dSocket.receive(inputPacket); //metodo bloccante
+                    String clientMessage = new String(inputPacket.getData(), 0, inputPacket.getLength());//offset: dove comincia la lettura del messaggio
                     System.out.println("\nClient: " + clientMessage);
                     InetAddress clientAddress = inputPacket.getAddress();
                     int port = inputPacket.getPort();
-                    System.out.println("Scrivi il messaggio da voler mandare al client: ");
+                    DatagramPacket clientPacket =  new DatagramPacket(clientMessage.getBytes(), clientMessage.length(), clientAddress, port);
+                    dSocket.send(clientPacket);
+                    System.out.println("messaggio rinviato");
+                    /*System.out.println("Scrivi il messaggio da voler mandare al client: ");
                     Scanner sc = new Scanner(System.in);
                     String message = sc.nextLine();
                     //prendo i bit, la lunghezza del messaggio, l'indirizzo del server e la porta
                     DatagramPacket outPacket = new DatagramPacket(message.getBytes(), message.length(), clientAddress, port);
-                    dSocket.send(outPacket);
+                    dSocket.send(outPacket);*/
                 }
                 System.out.println("Fine trasmissione");
+            }catch(UnknownHostException e){ //eccezione per input/output
+                System.err.println("Errore i/o");
+            }catch (SocketException e) {
+                System.err.println("Errore socket");
             } catch (IOException e) {
                 System.out.println("Errore");
             }
