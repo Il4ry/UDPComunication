d67010e613401c47ea2f31223db9f9ecc1eea409
diegoscuolaitis-tech <diegoscuolaitis@gmail.com>
2026-03-18 11:42:44 +0100

    18/03

--- src/MainServer.java
+++ src/MainServer.java
@@ -7,9 +7,10 @@ public class MainServer {
 
             try {
                 DatagramSocket dSocket = new DatagramSocket(3000);
-                byte[] bufferIn = new byte[256];
-                DatagramPacket inputPacket = new DatagramPacket(bufferIn, bufferIn.length);
-                while(bufferIn.length!=0) {
+
+                while(true) {
+                    byte[] bufferIn = new byte[256];
+                    DatagramPacket inputPacket = new DatagramPacket(bufferIn, bufferIn.length);
                     dSocket.receive(inputPacket); //metodo bloccante
                     String clientMessage = new String(inputPacket.getData(), 0, inputPacket.getLength());//offset: dove comincia la lettura del messaggio
                     System.out.println("\nClient: " + clientMessage);
@@ -18,6 +19,7 @@ public class MainServer {
                     DatagramPacket clientPacket =  new DatagramPacket(clientMessage.getBytes(), clientMessage.length(), clientAddress, port);
                     dSocket.send(clientPacket);
                     System.out.println("messaggio rinviato");
+                    InetAddress groupAddress = InetAddress.getByName("239.255.255.250");
                     /*System.out.println("Scrivi il messaggio da voler mandare al client: ");
                     Scanner sc = new Scanner(System.in);
                     String message = sc.nextLine();
@@ -25,7 +27,6 @@ public class MainServer {
                     DatagramPacket outPacket = new DatagramPacket(message.getBytes(), message.length(), clientAddress, port);
                     dSocket.send(outPacket);*/
                 }
-                System.out.println("Fine trasmissione");
             }catch(UnknownHostException e){ //eccezione per input/output
                 System.err.println("Errore i/o");
             }catch (SocketException e) {
